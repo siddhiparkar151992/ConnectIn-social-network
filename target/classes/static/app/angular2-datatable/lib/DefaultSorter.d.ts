8c7e426e66661c27a5d1eefb506e35cfd6ead026
siddhi_parkar <siddhiparkar15@gmail.com>
2017-02-04 14:16:21 -0500

    deleted target folder

--- target/classes/static/app/angular2-datatable/lib/DefaultSorter.d.ts
+++
@@ -1,9 +0,0 @@
-import { DataTable } from "./DataTable";
-export declare class DefaultSorter {
-    private mfTable;
-    private sortBy;
-    private isSortedByMeAsc;
-    private isSortedByMeDesc;
-    constructor(mfTable: DataTable);
-    private sort();
-}
