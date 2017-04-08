552a3bd11d58d0a9e63bce4df7d153e36889b720
siddhi_parkar <siddhiparkar15@gmail.com>
2017-04-08 08:12:12 -0400

    added header with profile dropdown and notification system

--- connectin-web/src/main/webapp/resources/static/app/modules/headers/profile/header.component.ts
+++ connectin-web/src/main/webapp/resources/static/app/modules/headers/profile/header.component.ts
@@ -1,4 +1,5 @@
 import {Component,OnInit} from "angular2/core";
+import {NotificationComponent} from "../../notifications/notifications.component";
 
 
 declare var $:any;
@@ -7,6 +8,7 @@ declare var $:any;
 	selector:'profile-header',
 	templateUrl: '/resources/static/app/modules/headers/profile/header.component.html',
 	providers:[],
+	directives:[NotificationComponent],
 	styleUrls: ['resources/styles/css/headers/profile-header.css']
 	
 })
---
+++ connectin-web/src/main/webapp/resources/static/app/modules/notifications/notifications.component.ts
@@ -0,0 +1,10 @@
+import {Component,OnInit} from "angular2/core";
+
+@Component({
+	selector:'notification-list',
+	templateUrl:'/resources/static/app/modules/notifications/notifications.component.html',
+	styleUrls: ['resources/styles/css/notifications/notifications.css']
+})
+export class NotificationComponent{
+	
+}
