cc8a2d3c89c507e64ddb5d915b4c21d5de00d79f
siddhi_parkar <siddhiparkar15@gmail.com>
2017-06-04 11:26:03 +0530

    fixed dependencies for maven projects

--- connectin-web/src/main/java/com/connectin/web/auth/AuthController.java
+++ connectin-web/src/main/java/com/connectin/web/auth/AuthController.java
@@ -1,15 +1,10 @@
 package com.connectin.web.auth;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.connectin.authenticate.entity.AuthErrors;
